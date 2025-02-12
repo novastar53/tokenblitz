6b9555611dba24f233ca5a0f9d15779ed2d5c3a9
Vikram Pawar <pvikram035@gmail.com>
2025-02-12 12:17:56 -0500

    Updated function names and implemented toy tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn say_hello(name: Option<&str>) -> PyResult<String> {
-    match name {
-        Some(n) => Ok(format!("Hello, {}!", n)),
-        None => Ok(String::from("Hello, World!"))
-    }
+fn tokenize(input: String) -> PyResult<Vec<String>> {
+    let words: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
+    Ok(words)
 }
 
 #[pymodule]
-fn hello_rust(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(say_hello, m)?)?;
+fn tokenblitz(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(tokenize, m)?)?;
     Ok(())
 }
